b3391b8dfbb4b395e10c595f8c91aad9c2571480
rmalnoult <romain.malnoult@gmail.com>
2015-03-25 18:20:22 +0100

    dynamic profile

--- src/Cooler/MainBundle/Controller/BeerController.php
+++ src/Cooler/MainBundle/Controller/BeerController.php
@@ -8,9 +8,47 @@ class BeerController extends Controller
 {
     public function indexAction($beerId)
     {
+    	$beer = $this->getBeerById($beerId);
+    	$style = $this->getBeerStyle($beer);
+    	$category = $this->getBeerCategory($beer);
 
         return $this->render('CoolerMainBundle:Profile:profile.html.twig', array(
-                // ...
-            ));    }
+                'beer' => $beer,
+                'style' => $style,
+                'category' => $category,
+            )
+        );    
+    }
+    public function getBeerById($beerId)
+    {
+    	$beerRepository = $this->getDoctrine()->getRepository('CoolerMainBundle:beers');
+    	$beer = $beerRepository->find($beerId);
+    	return $beer;
+    }
+    public function getBeerCategory($beer)
+    {
+    	$catid = $beer->getCatId();
+    	if ($catid != -1) {
+	    	$categoriesRepository = $this->getDoctrine()->getRepository('CoolerMainBundle:categories');
+	    	$category = $categoriesRepository->find($catid);
+	    	$category = $category->getCatName();
+	    	return $category;
+    	} else {
+    		return '';
+    	}
+
+    }
+	public function getBeerStyle($beer)
+    {
+    	$styleId = $beer->getStyleId();
+    	if ($styleId != -1) {
+    		$stylesRepository = $this->getDoctrine()->getRepository('CoolerMainBundle:styles');
+    		$style = $stylesRepository->find($styleId);
+    		$style = $style->getStyleName();
+    		return $style;
+    	} else {
+    		return '';
+    	}
+    }
 
 }
