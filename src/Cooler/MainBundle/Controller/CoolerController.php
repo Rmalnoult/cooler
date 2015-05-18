16deff8f837b0841ecb2587a87007a721e27b4c8
rmalnoult <romain.malnoult@gmail.com>
2015-05-18 12:38:15 +0200

    front

--- src/Cooler/MainBundle/Controller/CoolerController.php
+++ src/Cooler/MainBundle/Controller/CoolerController.php
@@ -4,8 +4,16 @@ namespace Cooler\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * Functions related to the application's cooler (collection of one user's beers)
+ */
 class CoolerController extends Controller
 {
+    /**
+     * parameter : $userId
+     * Get User's beers in an arrayCollection
+     * return the cooler view
+     */
     public function indexAction($userId)
     {
 
@@ -16,6 +24,11 @@ class CoolerController extends Controller
                 'beers' => $beers,
             ));    
     }
+    /**
+     * parameter : $userId
+     * Get User object from doctrine's user repository
+     * return user object
+     */
     public function getUserById($userId)
     {
         $userRepository = $this->getDoctrine()->getRepository('CoolerUserBundle:User');
